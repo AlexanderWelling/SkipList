e4d245ad32c238d4d78454f92b22b56ed5de534f
Jan Alexander <s-jwelli@if016-ws07.if.haw-landshut.de>
2018-06-06 16:13:35 +0200

    Initial commit

---
+++ src/com/company/SkipElement.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class SkipElement<T extends Comparable<T>> {
+    final private T value;
+    private SkipElement<T> next;
+    private SkipElement<T> nextLevel;
+
+    public SkipElement(T value, SkipElement<T> next, SkipElement<T> nextLevel)
+    {
+        this.value = value;
+        this.next = next;
+        this.nextLevel = nextLevel;
+    }
+
+    public SkipElement(T value, SkipElement<T> nextValue)
+    {
+        this(value, nextValue, null);
+    }
+
+    public SkipElement(T value)
+    {
+        this(value, null);
+    }
+
+    public SkipElement()
+    {
+        this(null);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public SkipElement<T> getNext() {
+        return next;
+    }
+
+    public SkipElement<T> getNextLevel() {
+        return nextLevel;
+    }
+
+    public void setNext(SkipElement<T> next) {
+        this.next = next;
+    }
+
+    public void setNextLevel(SkipElement<T> nextLevel) {
+        this.nextLevel = nextLevel;
+    }
+
+    public boolean isSentinal()
+    {
+        return value == null;
+    }
+}
---
+++ src/com/company/SkipList.java
@@ -0,0 +1,194 @@
+package com.company;
+
+import java.util.*;
+
+public class SkipList<T extends Comparable<T>> implements List<T>{
+    final static double PROBABILITY = 0.35;
+    final static int EPSILON = 2;
+    private SkipElement firstSentinal;
+    private SkipElement lastSentinal;
+    final private double probability;
+    final private int maxLevel;
+
+
+    public SkipList(double probability, int maxLevel)
+    {
+        this.firstSentinal = new SkipElement();
+        this.lastSentinal = new SkipElement();
+
+        firstSentinal.setNext(lastSentinal);
+
+        SkipElement currFirstSen =  this.firstSentinal;
+        SkipElement currLastSen =  this.lastSentinal;
+        for(int i = 0; i < maxLevel; i++)
+        {
+            currFirstSen.setNextLevel(new SkipElement());
+            currLastSen.setNextLevel(new SkipElement());
+
+            currFirstSen = currFirstSen.getNextLevel();
+            currLastSen = currLastSen.getNextLevel();
+
+            currFirstSen.setNext(currLastSen);
+        }
+
+        this.probability = probability;
+        this.maxLevel = maxLevel;
+    }
+
+    public SkipList(int expectedElements){
+        this(PROBABILITY, (int)Math.ceil(Math.log(expectedElements)/Math.log(1/PROBABILITY)) + EPSILON);
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        SkipElement currentElement = firstSentinal;
+
+        while(currentElement.getNextLevel() != null)
+        {
+            if(!currentElement.getNext().isSentinal() || currentElement.getNext().getValue().compareTo(t) < 0)
+            {
+                currentElement = currentElement.getNext();
+            }
+            else currentElement = currentElement.getNextLevel();
+        }
+
+        currentElement.setNext(new SkipElement(t, currentElement.getNext()));
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+        SkipElement currentElement = firstSentinal;
+
+        while(currentElement.getNextLevel() != null)
+        {
+            if(!currentElement.getNext().isSentinal() || currentElement.getNext().getValue().compareTo(o) != 0)
+            {
+                currentElement = currentElement.getNext();
+            }
+            else currentElement = currentElement.getNextLevel();
+        }
+        currentElement.setNext(currentElement.getNext().getNext());
+        return true;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+
+
+    public T getFirst()
+    {
+
+    }
+
+    public T getLast()
+    {
+
+    }
+}
